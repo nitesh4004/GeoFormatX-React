36a43b351781c75242162bb0a659b9692878ccf2
Nitesh Kumar <111670351+nitesh4004@users.noreply.github.com>
2026-01-10 01:38:27 +0530

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+// Component stubs for GeoFormatX
+export { default as AdminBoundaries } from './AdminBoundaries';
+export { default as PostalCodes } from './PostalCodes';
+export { default as ParliamentBoundaries } from './ParliamentBoundaries';
+export { default as RiverAnalysis } from './RiverAnalysis';
+export { default as FormatConverter } from './FormatConverter';
+export { default as VectorCalculator } from './VectorCalculator';
