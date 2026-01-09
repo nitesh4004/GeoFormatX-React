35d6064c388ecbe05e4cf73f22a9972a4ae34f7f
Nitesh Kumar <111670351+nitesh4004@users.noreply.github.com>
2026-01-10 01:56:06 +0530

    Create VectorCalculator.tsx

---
+++ src/components/VectorCalculator.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const VectorCalculator: React.FC = () => <div className="p-4"><h2>Vector Calculator</h2><p>Perform vector analysis on geographic data.</p></div>;
+export default VectorCalculator;
