633ab6a6335cdc525cfcd67c4aa25adc65889e67
Nitesh Kumar <111670351+nitesh4004@users.noreply.github.com>
2026-01-10 01:57:16 +0530

    Add component exports for all sections

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@
 export { default as AdminBoundaries } from './AdminBoundaries';
 export { default as PostalCodes } from './PostalCodes';
 export { default as ParliamentBoundaries } from './ParliamentBoundaries';
-export { default as RiverAnalysis } from './RiverAnalysis';
-export { default as FormatConverter } from './FormatConverter';
+export { default as Rivers } from './Rivers';
+export { default as Converter } from './Converter';
 export { default as VectorCalculator } from './VectorCalculator';
