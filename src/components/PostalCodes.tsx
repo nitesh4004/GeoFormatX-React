90947ae96e377b9b4cca5c950a4617c6ead69f4d
Nitesh Kumar <111670351+nitesh4004@users.noreply.github.com>
2026-01-10 01:54:37 +0530

    Create PostalCodes.tsx

---
+++ src/components/PostalCodes.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostalCodes: React.FC = () => {
+  return <div className="p-4"><h2>Postal Codes</h2><p>Postal code information will be displayed here.</p></div>;
+};
+
+export default PostalCodes;
