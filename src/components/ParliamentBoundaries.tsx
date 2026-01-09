bf021f9b2c48d5588d7fce476928c85e358c6ad5
Nitesh Kumar <111670351+nitesh4004@users.noreply.github.com>
2026-01-10 01:55:00 +0530

    Create ParliamentBoundaries.tsx

---
+++ src/components/ParliamentBoundaries.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const ParliamentBoundaries: React.FC = () => <div className="p-4"><h2>Parliament Boundaries</h2><p>Parliamentary boundaries data.</p></div>;
+export default ParliamentBoundaries;
